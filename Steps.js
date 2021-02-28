82f1e924f0792072952d413a64f8bed86b0a69e0
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-02-28 18:10:52 +0545

    Added A11 -1 multiplication case

--- Steps.js
+++ Steps.js
@@ -26,22 +26,32 @@ const fundamentalStep = (questionData) => {
             combinedReason = combinedReason + data + (index === (reason.length - 1) ? String.raw`` : commaValue);
 
         })
-        let finalLatex = String.raw`\sim \text{ }\left| \text{ }\begin{matrix}${rowData[0][0]} & ${rowData[0][1]} & ${rowData[0][2]} \\${rowData[1][0]} & ${rowData[1][1]} & ${rowData[1][2]} \\${rowData[2][0]} & ${rowData[2][1]} & ${rowData[2][2]} \\ \end{matrix}\text{ }\begin{matrix}: \\: \\: \\ \end{matrix} \right.\text{ }\left. \begin{matrix}${rowData[0][3]} \\${rowData[1][3]} \\${rowData[2][3]} \\ \end{matrix}\text{ } \right|\text{ }` + combinedReason;
-
-        returnObject.latexArray.push(finalLatex);
-
+        returnObject.latexArray.push(String.raw`\sim \text{ }\left| \text{ }\begin{matrix}${rowData[0][0]} & ${rowData[0][1]} & ${rowData[0][2]} \\${rowData[1][0]} & ${rowData[1][1]} & ${rowData[1][2]} \\${rowData[2][0]} & ${rowData[2][1]} & ${rowData[2][2]} \\ \end{matrix}\text{ }\begin{matrix}: \\: \\: \\ \end{matrix} \right.\text{ }\left. \begin{matrix}${rowData[0][3]} \\${rowData[1][3]} \\${rowData[2][3]} \\ \end{matrix}\text{ } \right|\text{ }` + combinedReason);
     }
     return returnObject;
 }
 
 // In the first step, we will try to make A11 as 1
-const firstStep = (returnObject) => {
+const firstStep = ({ latexArray, questionData }) => {
+    let reason;
+    let returnObject = {
+        latexArray: latexArray,
+        questionData: questionData
+    };
+
+    // If the A11 has -1 then it will be simply converted to 1 by multiplying with -1
+    if (returnObject.questionData[0][0] === -1) {
+        returnObject.questionData[0] = [returnObject.questionData[0][0] * (-1), returnObject.questionData[0][1] * (-1), returnObject.questionData[0][2] * (-1), returnObject.questionData[0][3] * (-1)];
+        reason = String.raw`\small{{{\text{R}}_{\text{${1}}}}\to {{\text{R}}_{\text{${1}}}} \times(-1)}`;
+        let rowData = returnObject.questionData;
+        returnObject.latexArray.push(String.raw`\sim \text{ }\left| \text{ }\begin{matrix}${rowData[0][0]} & ${rowData[0][1]} & ${rowData[0][2]} \\${rowData[1][0]} & ${rowData[1][1]} & ${rowData[1][2]} \\${rowData[2][0]} & ${rowData[2][1]} & ${rowData[2][2]} \\ \end{matrix}\text{ }\begin{matrix}: \\: \\: \\ \end{matrix} \right.\text{ }\left. \begin{matrix}${rowData[0][3]} \\${rowData[1][3]} \\${rowData[2][3]} \\ \end{matrix}\text{ } \right|\text{ }` + reason);
+        return returnObject;
+    }
+
+    // If it's -1 then above if statement will run, else for every value that's not 1, we will make it 1 in the if
+    // statement below
     if (returnObject.questionData[0][0] !== 1) {
-        let reason = [];
-        let returnObject = {
-            latexArray: latexArray,
-            questionData: questionData
-        };
+        // R1 -> R1 + xR2 or R1 -> R1 - xR2, here R1 is to be made 1
         return returnObject;
     }
     // If the A11 equals to 1 then, simply pass on the returnObject to another step
