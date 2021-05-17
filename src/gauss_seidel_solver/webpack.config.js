cf76c0190dd80b433e28a8608cc799759dd46a17
rishadbaniya <rbdroid01@gmail.com>
2021-05-17 07:59:44 +0545

    made release folder

--- src/gauss_seidel_solver/webpack.config.js
+++ src/gauss_seidel_solver/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './solve.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'gauss_seidel_solver.js',
+    path: path.resolve(__dirname, 'release'),
   },
 };
