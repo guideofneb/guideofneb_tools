f4103f1c9a456f78d05dba0b63f67fcbc4292919
rishadbaniya <rbdroid01@gmail.com>
2021-05-22 22:41:39 +0545

    Added web server

--- index.ts
+++ index.ts
@@ -1,26 +1,22 @@
-import * as GUIDEONEB_TOOLS from './src/exports.ts';
+import { serve } from "https://deno.land/std@0.97.0/http/server.ts";
 import * as WRAPPER from './worker_wrapper.js';
 
 const _WORKER0 = new WRAPPER.WorkerWrapper();
 const _WORKER1 = new WRAPPER.WorkerWrapper();
 
-const solver = ()=>{
-    let a = performance.now();
-    for (let x = 0; x<1000; x++){
-        if(x%2 === 0){
-           _WORKER0.solve("GAUSS_SIEDEL", {equation1:`2x+y=${x}`,equation2: "x+3y+10z=12",equation3:"10z=2"}).then(()=>{
-                if(x === 9999){
-                    console.log(performance.now()-a);
-                };
-          });
-        }else{
-           _WORKER1.solve("GAUSS_SIEDEL", {equation1:`2x+y=${x}`,equation2: "x+3y+10z=12",equation3:"10z=2"}).then(()=>{
-                if(x === 9999){
-                    console.log(performance.now()-a);
-                };
-          });
-        }
-    }
-}
 
-solver();
+const server = serve({ port: 8080 });
+for await (const request of server) {
+  if(request.method === "GET"){
+    console.log(request.method)
+    let bodyContent = "Your user-agent is:\n\n";
+    const a = performance.now();
+    _WORKER1.solve("GAUSS_SIEDEL", {equation1:`2x+y=10`,equation2: "x+3y+10y=12",equation3:"10z=2"}).then((v)=>{
+      bodyContent += v;
+      console.log(performance.now()-a);
+      request.respond({ status: 200, body: bodyContent });
+  });
+  }
+
+
+}
