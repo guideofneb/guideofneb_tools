c95f15adf71781296c35f3eb781a2b89d7f45804
rishadbaniya <rbdroid01@gmail.com>
2021-05-23 11:51:48 +0545

    Added web server

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ const _WORKER1 = new WRAPPER.WorkerWrapper();
 
 const server = serve({ port: 8080 });
 for await (const request of server) {
-  if(request.method === "GET"){
+  if(request.method === "POST"){
     console.log(request.method)
     let bodyContent = "Your user-agent is:\n\n";
     const a = performance.now();
-    _WORKER1.solve("GAUSS_SIEDEL", {equation1:`2x+y=10`,equation2: "x+3y+10y=12",equation3:"10z=2"}).then((v)=>{
-      bodyContent += v;
-      console.log(performance.now()-a);
-      request.respond({ status: 200, body: bodyContent });
+      _WORKER1.solve("GAUSS_SIEDEL", {equation1:`2x+y=10`,equation2: "x+3y+10y=12",equation3:"10z=2"}).then((v)=>{
+       bodyContent += v;
+       console.log(performance.now()-a);
+       request.respond({ status: 200, body: bodyContent });
   });
   }
 
