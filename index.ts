5313c3536d6f797a4a0e3fb50acc8cb2d89721c9
rishadbaniya <rbdroid01@gmail.com>
2021-05-26 17:04:27 +0545

    Added round robin

--- index.ts
+++ index.ts
@@ -4,18 +4,28 @@ import * as WRAPPER from './worker_wrapper.js';
 const _WORKER0 = new WRAPPER.WorkerWrapper();
 const _WORKER1 = new WRAPPER.WorkerWrapper();
 
-
 const server = serve({ port: 8080 });
+let aa = 1;
+let currentWorker = 0;
 for await (const request of server) {
   if(request.method === "POST"){
-    console.log(request.method)
-    let bodyContent = "Your user-agent is:\n\n";
     const a = performance.now();
-      _WORKER1.solve("GAUSS_SIEDEL", {equation1:`2x+y=10`,equation2: "x+3y+10y=12",equation3:"10z=2"}).then((v)=>{
-       bodyContent += v;
+    if(currentWorker === 0){
+      _WORKER1.solve("GAUSS_SIEDEL", {equation1:`2x+y=10`,equation2: "x+3y+10y=12",equation3:"10z=2"}).then((d)=>{
+       const bodyContent = d;
        console.log(performance.now()-a);
        request.respond({ status: 200, body: bodyContent });
-  });
+    });
+      currentWorker = 1;
+    }else {
+      _WORKER0.solve("GAUSS_SIEDEL", {equation1:`2x+y=10`,equation2: "x+3y+10y=12",equation3:"10z=2"}).then((d)=>{
+       const bodyContent = d;
+       console.log(performance.now()-a);
+       request.respond({ status: 200, body: bodyContent });
+    });
+      currentWorker = 0;
+    }
+
   }
 
 
